f2acd0c142bc6c3f783c1e9b9cd6c70e7b28671b
Will Chelberg <willchelberg@gmail.com>
2026-03-01 01:04:27 -0600

    Fix import paths in main.tsx

--- main.tsx
+++ main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./styles/globals.css";
+import App from "./src/App.tsx";
+import "./src/styles/globals.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+
