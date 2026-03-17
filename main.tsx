3a7e1db402c176c519dd56c83140090f41c81e78
Will Chelberg <willchelberg@gmail.com>
2026-03-16 23:33:07 -0600

    Fix CSS import path

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./src/App.tsx";
-import "./src/styles/globals.css";
+import "./styles/globals.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
 
